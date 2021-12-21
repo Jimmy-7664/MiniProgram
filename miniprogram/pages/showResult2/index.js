78ffd5f36daaba729171517b9ae4b18d355ea305
CuiYuSong <2926969535@qq.com>
2021-12-21 21:09:15 +0800

    change gif

--- miniprogram/pages/showResult2/index.js
+++ miniprogram/pages/showResult2/index.js
@@ -8,29 +8,45 @@ Page({
     haveGetRecord: false,
     envId: '',
     record: '',
-    type: 1
+    type: ''
   },
 
-    onLoad: function (query) {
-      console.log(query);
+  onLoad: function (query) {
+    console.log(query);
+
+    if (query.human=="true" & query.avoid=="true") {
       this.setData({
-        type:query.type
-      });
-    },
-  
+        type:3
+      })
+    }
+    if (query.human == "true"  & query.avoid=="false") {
+      this.setData({
+        type:2
+      })    }
+    if (query.human == "false" & query.avoid=="true") {
+      this.setData({
+        type:1
+      })    
+    }
+    // this.setData({
+    //   type: query.human
+    // });
+    console.log(this.data.type);
+
+  },
+
 
   goBack() {
-    let rand1=Math.random();
-      console.log("2");
-      if (rand1 < 0.5) {
-        wx.redirectTo({
-          url: `/pages/experiment/index?envId=jimmy-gao-622a98`,
-        });
-      } else {
-        wx.redirectTo({
-          url: `/pages/experiment2/index?envId=jimmy-gao-622a98`,
-        });
-      }
+    let rand1 = Math.random();
+    if (rand1 < 0.5) {
+      wx.redirectTo({
+        url: `/pages/experiment/index?envId=jimmy-gao-622a98`,
+      });
+    } else {
+      wx.redirectTo({
+        url: `/pages/experiment2/index?envId=jimmy-gao-622a98`,
+      });
+    }
   },
 
 });
